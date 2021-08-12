ffa93a7b07c3f0c3330abf640c933ecd3d843d3b
Finn Gruwier Larsen <finn@gruwier.dk>
2021-08-12 13:21:09 +0200

    Build the command.

--- run_chewiesnake.py
+++ run_chewiesnake.py
@@ -1,7 +1,6 @@
 import os
 import argparse
 import pathlib
-import json
 import subprocess
 
 CHEWIESNAKE_IMAGE_ID = os.getenv("CHEWIESNAKE_IMAGE_ID")
@@ -12,14 +11,31 @@ parser = argparse.ArgumentParser(description='Run chewieSnake with selected samp
 parser.add_argument('-s','--sample_names', nargs='+', help='Sample names (strip read numbers and extension from file names).')
 args = parser.parse_args()
 
-lines = ["sample\tfq1\tfq2"]
+lines = ["sample\tfq1\tfq2\n"]
 
 for sample_name in args.sample_names:
     file_name_1 = f"{sample_name}_R1_001.fastq.gz"
     file_name_2 = f"{sample_name}_R2_001.fastq.gz"
-    line = f"{sample_name}\t/chewieSnake/analysis/{file_name_1}\t{file_name_2}"
+    line = f"{sample_name}\t/chewieSnake/analysis/{file_name_1}\t/chewieSnake/analysis/{file_name_2}"
     lines.append(line)
 
-with open('samples.tsv', 'w') as file:
+mount_point = pathlib.Path(CHEWIESNAKE_MOUNT_POINT)
+assert mount_point.exists()
+samples_tsv_path = pathlib.Path(mount_point, 'samples.tsv')
+
+with open(samples_tsv_path, 'w') as file:  # Todo: files skal lægges i CHEWIESNAKE_MOUNT_POINT!
     file.writelines(lines)
     file.write("\n")
+
+command = f"""docker run --rm \
+-v {CHEWIESNAKE_MOUNT_POINT}:/chewieSnake/analysis \
+-e LOCAL_USER_ID=$(id -u $USER) \
+{CHEWIESNAKE_IMAGE_ID} \
+--reads \
+--sample_list /chewieSnake/analysis/samples.tsv \
+--scheme /chewieSnake/analysis/enterobase_senterica_cgmlst \
+--prodigal /chewieSnake/analysis/Salmonella_enterica.trn \
+--working_directory /chewieSnake/analysis/output
+"""
+print("Command:")
+print(command)
