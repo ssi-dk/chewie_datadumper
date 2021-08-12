262e5d8155e446bcfb112fc0826b88826c4715bf
Finn Gruwier Larsen <finn@gruwier.dk>
2021-08-12 15:06:10 +0200

    Add constant PATH_INSIDE_CONTAINER.

--- run_chewiesnake.py
+++ run_chewiesnake.py
@@ -5,6 +5,7 @@ import subprocess
 
 CHEWIESNAKE_IMAGE_ID = os.getenv("CHEWIESNAKE_IMAGE_ID")
 CHEWIESNAKE_MOUNT_POINT = os.getenv("CHEWIESNAKE_MOUNT_POINT")
+PATH_INSIDE_CONTAINER = "/chewieSnake/analysis"  # Should not be changed as this is what chewieSnake expects
 BIFROST_DB_KEY = os.getenv("BIFROST_DB_KEY", "mongodb://localhost/bifrost_test")
 
 parser = argparse.ArgumentParser(description='Run chewieSnake with selected samples and save allele profiles to MongoDB.')
@@ -16,7 +17,7 @@ lines = ["sample\tfq1\tfq2\n"]
 for sample_name in args.sample_names:
     file_name_1 = f"{sample_name}_R1_001.fastq.gz"
     file_name_2 = f"{sample_name}_R2_001.fastq.gz"
-    line = f"{sample_name}\t/chewieSnake/analysis/{file_name_1}\t/chewieSnake/analysis/{file_name_2}"
+    line = f"{sample_name}\t{PATH_INSIDE_CONTAINER}/{file_name_1}\t{PATH_INSIDE_CONTAINER}/{file_name_2}"
     lines.append(line)
 
 mount_point = pathlib.Path(CHEWIESNAKE_MOUNT_POINT)
@@ -28,14 +29,14 @@ with open(samples_tsv_path, 'w') as file:
     file.write("\n")
 
 command = f"""docker run --rm \
--v {CHEWIESNAKE_MOUNT_POINT}:/chewieSnake/analysis \
+-v {CHEWIESNAKE_MOUNT_POINT}:{PATH_INSIDE_CONTAINER} \
 -e LOCAL_USER_ID=$(id -u $USER) \
 {CHEWIESNAKE_IMAGE_ID} \
 --reads \
---sample_list /chewieSnake/analysis/samples.tsv \
---scheme /chewieSnake/analysis/enterobase_senterica_cgmlst \
---prodigal /chewieSnake/analysis/Salmonella_enterica.trn \
---working_directory /chewieSnake/analysis/output
+--sample_list {PATH_INSIDE_CONTAINER}/samples.tsv \
+--scheme {PATH_INSIDE_CONTAINER}/enterobase_senterica_cgmlst \
+--prodigal {PATH_INSIDE_CONTAINER}/Salmonella_enterica.trn \
+--working_directory {PATH_INSIDE_CONTAINER}/output
 """
 print("Command:")
 print(command)
