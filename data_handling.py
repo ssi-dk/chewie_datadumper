7dd6cdbb795aadac90e233610bde177e8a0c301f
Finn Gruwier Larsen <finn@gruwier.dk>
2021-08-13 14:29:03 +0200

    tsv_reader > line_reader.

--- data_handling.py
+++ data_handling.py
@@ -1,4 +1,4 @@
-def tsv_reader(file_name):
+def line_reader(file_name):
     """Get lines from text file one by one using a generator object
     """
     with open(file_name, 'r') as file_handler:
@@ -7,10 +7,11 @@ def tsv_reader(file_name):
 
 
 def write_allele_profiles_to_mongo(file):
-    allele_profile_reader = tsv_reader(file)
+    allele_profile_reader = line_reader(file)
     next(allele_profile_reader)  # Skip header line
     for line in allele_profile_reader:
         allele_profile = line.split('\t')
         sample_name = allele_profile.pop(0)
         print("Sample name: ", sample_name)
         print("Length of allele profile:", len(allele_profile))
+        print(type(allele_profile[0]))
